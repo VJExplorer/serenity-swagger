839ea4d5fedf50556f0c76b261008d790598583c
VJExplorer <varuni_j@yahoo.com>
2023-08-10 09:16:23 +0100

    login as standard user added

---
+++ src/test/java/starter/stepdefinitions/AuthenticationStepDefinition.java
@@ -0,0 +1,33 @@
+package starter.stepdefinitions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.Text;
+import swaglabs.model.SwaglabsUser;
+import swaglabs.tasks.athentication.Login;
+
+
+public class AuthenticationStepDefinition {
+
+    @Given("{actor} is on the login page")
+    public void on_the_login_page(Actor actor){
+        actor.attemptsTo(Open.url("https://www.saucedemo.com/"));
+
+    }
+    @When("{actor} logs in with valid credentials")
+    public void logs_in_with_valid_credentials(Actor actor) {
+        actor.attemptsTo(
+                Login.asA(SwaglabsUser.STANDARD_USER)
+
+        );
+    }
+
+    @Then("{actor} should be presented with the product catalog")
+    public void should_be_presented_with_product_catalog(Actor actor) {
+        actor.attemptsTo(
+                Ensure.that(Text.of(".title")).isEqualToIgnoringCase("Products"));
+    }
+}
